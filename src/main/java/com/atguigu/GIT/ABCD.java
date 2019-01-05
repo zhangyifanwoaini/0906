fc0a670fea6e970a3fb74068667290af849fd646
xiangyun <xiangyun@wenwen.com>
2019-01-05 20:44:10 +0800

    这是我晚上上传的

---
+++ src/main/java/com/atguigu/GIT/ABCD.java
@@ -0,0 +1,13 @@
+package com.atguigu.GIT;
+
+public class ABCD {
+
+    public static void main(String[] args) {
+        System.out.println("11111111");
+        System.out.println("22222222");
+        System.out.println("333333333");
+        System.out.println("444444444");
+        System.out.println("5555555555");
+        System.out.println("6666666666");
+    }
+}
